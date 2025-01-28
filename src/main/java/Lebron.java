3f9ed51865b9300ea1f6ef9ad95ad2b7a057a500
Jia Jun <jiajun412@gmail.com>
2025-01-28 20:01:50 +0800

    Updated code to echo user input

--- src/main/java/Lebron.java
+++ src/main/java/Lebron.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
+
 public class Lebron {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Lebron");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
+        String lineBreak = "____________________________________________________________";
+        System.out.println("Hello! I'm Lebron, just a little kid from Akron");
+        System.out.println("What we doing today coach?");
+        System.out.println(lineBreak);
+
+        String line;
+        while(true) {
+            Scanner input = new Scanner(System.in);
+            line = input.nextLine();
+            if (line.equals("bye")) {
+                System.out.println("Aight catch you later, see you at the game!");
+                break;
+            }
+            System.out.println(line);
+            System.out.println(lineBreak);
+        }
     }
 }
