e09d8b9c36c2b10879688f37309a28425ad524e1
Jia Jun <jiajun412@gmail.com>
2025-02-01 20:48:15 +0800

    Added add and list

--- src/main/java/Lebron.java
+++ src/main/java/Lebron.java
@@ -1,22 +1,43 @@
 import java.util.Scanner;
 
 public class Lebron {
-    public static void main(String[] args) {
-        String lineBreak = "____________________________________________________________";
+    private static final String LINEBREAK = "____________________________________________________________";
+    private static String[] tasks = new String[100];
+    private static int tasksSize = 0;
+
+    public static void greetingMessage() {
         System.out.println("Hello! I'm Lebron, just a little kid from Akron");
         System.out.println("What we doing today coach?");
-        System.out.println(lineBreak);
+        System.out.println(LINEBREAK);
+    }
 
-        String line;
+    public static void addTask(String task) {
+        tasks[tasksSize] = task;
+        tasksSize++;
+        System.out.println("Alright! Added " + task);
+        System.out.println(LINEBREAK);
+    }
+
+    public static void printTasks() {
+        for (int i = 0; i < tasksSize; i++) {
+            System.out.println((i + 1) + ". " + tasks[i]);
+        }
+        System.out.println(LINEBREAK);
+    }
+
+    public static void main(String[] args) {
+        greetingMessage();
         while(true) {
             Scanner input = new Scanner(System.in);
-            line = input.nextLine();
+            String line = input.nextLine();
             if (line.equals("bye")) {
                 System.out.println("Aight catch you later, see you at the game!");
                 break;
+            } else if (line.equals("list")) {
+                printTasks();
+            } else {
+                addTask(line);
             }
-            System.out.println(line);
-            System.out.println(lineBreak);
         }
     }
 }
